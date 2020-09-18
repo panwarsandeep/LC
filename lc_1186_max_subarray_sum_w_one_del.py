429f5dc5e02ff3f6f85fc6b9524ee77d22148dd4
Sandeep <panwarsandeep@gmail.com>
2020-09-18 21:57:53 +0530

    LC 1186
    https://leetcode.com/problems/maximum-subarray-sum-with-one-deletion/

--- lc_1186_max_subarray_sum_w_one_del.py
+++ lc_1186_max_subarray_sum_w_one_del.py
@@ -2,32 +2,34 @@ from collections import defaultdict
 import sys
 class Solution:
     def maximumSum(self, arr):
-        def msum(arr):
-        
-            if not arr:
-                return 0
-            sum = arr[0]
-            max_so_far = arr[0]
+        maxs = -sys.maxsize
+        for v in arr:
+            if v > maxs:
+                maxs = v
+        if maxs < 0:
+            return maxs
 
-            for i in range(1, len(arr)):
-                if sum + arr[i] >= 0:
-                    sum += arr[i]
-                    if max_so_far < sum:
-                        max_so_far = sum
-                else:
-                    sum = arr[i] if arr[i] > 0 else 0
+        fwd_path = []
+        sum = 0
+        full_max = -sys.maxsize
+        for v in arr:
+            sum = max(v, v + sum)
+            fwd_path.append(sum)
+            full_max = sum if sum > full_max else full_max
 
-            return max_so_far
-        totsum = msum(arr)
-        max_so_far = totsum
-        print(max_so_far)
-        for i in range(1, len(arr)):
-            tsum = msum(arr[:i])+ msum(arr[i+1:])
-            print(tsum, arr[:i], arr[i+1:])
-            if tsum > max_so_far:
-                max_so_far = tsum
+        rev_path = []
+        sum = 0
+        for v in arr[::-1]:
+            sum = max(v, v + sum)
+            rev_path.append(sum)
 
-        return max_so_far
+        rev_path.reverse()
+
+        ans = -sys.maxsize
+        for i in range(1, len(arr)-1):
+            tmp = max(full_max, fwd_path[i-1] + rev_path[i+1])
+            ans = tmp if tmp > ans else ans
+        return ans
         
 
 
@@ -37,9 +39,10 @@ if __name__ == '__main__':
     arr = [3, -2, 2, 3]
     arr = [-1, -1, -1, -1]
     #arr = [1,-2,-2,3]
-    arr = [-7,6,1,2,1,4,-1]
-    arr = [0,-5,-6,5,0,-5]
+    #arr = [-7,6,1,2,1,4,-1]
+    #arr = [0,-5,-6,5,0,-5]
     #arr = [1,-2,0,3]
-    arr = [-5,8,-7,12,7,2,-11,6,-1,2,-1,8,-1,0,1]
+    #arr = [-5,8,-7,12,7,2,-11,6,-1,2,-1,8,-1,0,1]
+    print(arr)
     r = sol.maximumSum(arr)
     print(r)
