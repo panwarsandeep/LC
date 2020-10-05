487d2cdc9ee1cd13388379b87754896c39fe2566
Sandeep <panwarsandeep@gmail.com>
2020-10-05 18:44:24 +0530

    LC 746
    improvised

--- lc_746_min_cost_clibing_stairs.py
+++ lc_746_min_cost_clibing_stairs.py
@@ -2,28 +2,20 @@ from collections import defaultdict
 import sys
 class Solution:
     def minCostClimbingStairs(self, cost):
-        def calc_cost(i):
-            if i > len(cost) -1:
-                return 0
-            if dp[i] != -1:
-                return dp[i]
-
-            tcost = cost[i]
-            dp[i] =  min(calc_cost(i+1), calc_cost(i+2)) + tcost
-            
-            return dp[i]
-    
-        dp = [-1]*len(cost)
-        return min(calc_cost(0), calc_cost(1))
-
-
+        first, second = cost[0], cost[1]
+        for i in range(2, len(cost)):
+            tmp = second
+            second = min(first, second) + cost[i]
+            first = tmp
+        
+        return min(first, second)
 
  
 if __name__ == '__main__':
     sol = Solution()
     
     cost = [10, 15, 20]
-    cost = [1, 100, 1, 1, 1, 100, 1, 1, 100, 1]
+    #cost = [1, 100, 1, 1, 1, 100, 1, 1, 100, 1]
     
     print(cost)
     r = sol.minCostClimbingStairs(cost)
