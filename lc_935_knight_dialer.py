4efa2960dda3fd76587d44b7acd92c1fca6c05e4
Sandeep <panwarsandeep@gmail.com>
2020-10-06 18:01:31 +0530

    LC 935
    https://leetcode.com/problems/knight-dialer/

--- lc_935_knight_dialer.py
+++ lc_935_knight_dialer.py
@@ -6,6 +6,24 @@ class Solution:
         dmap = [[4, 6], [8, 6], [7, 9], [8, 4], [3, 9, 0], [], [1, 7, 0], [2, 6], [1, 3], [2, 4],[4, 6]]
         mod_val = 10 ** 9 + 7
         ans = 0
+        dp1 = [1]*10
+        dp2 = [0]*10
+        
+        for _ in range(2, n+1):
+            for j in range(0, 10):
+                tv = dmap[j]
+                for t in tv:
+                    dp2[j] += dp1[t] % mod_val
+            dp1 = dp2[:]
+            dp2 = [0]*10
+        #print(dp)
+        ans = sum(dp1) % mod_val
+        return ans
+
+        '''
+        dmap = [[4, 6], [8, 6], [7, 9], [8, 4], [3, 9, 0], [], [1, 7, 0], [2, 6], [1, 3], [2, 4],[4, 6]]
+        mod_val = 10 ** 9 + 7
+        ans = 0
         dp = [[0]*10 for _ in range(n+1)]
         for i in range(10):
             dp[1][i] = 1
@@ -17,6 +35,7 @@ class Solution:
         #print(dp)
         ans = sum(dp[n]) % mod_val
         return ans
+        '''
         
                 
 
@@ -27,7 +46,7 @@ if __name__ == '__main__':
     sol = Solution()
     
     n = 4
-    #n = 3131
+    n = 3131
     r = sol.knightDialer(n)
     print(r)
     
