e9d4b2af3c1c41c00f9efe5fe2df081bc5df4d46
Sandeep <panwarsandeep@gmail.com>
2020-10-25 22:41:55 +0530

    LC 124

--- lc_124_binary_tree_max_path_sum.py
+++ lc_124_binary_tree_max_path_sum.py
@@ -20,10 +20,10 @@ class Solution:
     similarly max right (for parent node 20) would be 7.
     now if the tree rooted at 20 has the max path then it would include the sum of left, right and root which would be: 15 + 20 + 7 = 42, in this case
     the path is 15 -> 20 -> 7 (each path is traced exactly once)
-    if not, then we need to move up, but in that case we can take both 15, and 7 because that will not be a valid path according to the problem statement
+    if not, then we need to move up, but in that case we can not take both 15, and 7 because that will not be a valid path according to the problem statement
     the reason is the path should not be retraced e.g. path involving -10 could be: -10 -> 20 -> 15 or -10 -> 20 -> 7. we can't include both 15 and 7
     in single path including node -10. For this reason, if we want to move to parent of any node, take the max of its left and right and include that.
-    
+
     '''
     def maxPathSum(self, root):
         self.result = -sys.maxsize
