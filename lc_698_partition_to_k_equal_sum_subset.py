48eb0c03d6402a4e9ceb867a024d28bc87ad4993
Sandeep <panwarsandeep@gmail.com>
2020-10-31 22:07:05 +0530

    LC 698
    https://leetcode.com/problems/partition-to-k-equal-sum-subsets/

---
+++ lc_698_partition_to_k_equal_sum_subset.py
@@ -0,0 +1,30 @@
+class Solution:
+    def canPartitionKSubsets(self, nums, k):
+        def partitioin_helper(st, nums, visited, k, sum_so_far, tsum):
+            if k == 1:
+                return True
+            
+            if sum_so_far == tsum:
+                return partitioin_helper(0, nums, visited, k-1, 0, tsum)
+            
+            for i in range(st, len(nums)):
+                if visited[i] == False:
+                    visited[i] = True
+                    if partitioin_helper(i+1, nums, visited, k, sum_so_far+nums[i], tsum):
+                        return True
+                    visited[i] = False
+            return False
+
+        if k == 0 or sum(nums) % k != 0:
+            return False
+        tsum = sum(nums) // k
+        visited = [False]*len(nums)
+        return partitioin_helper(0, nums, visited, k, 0, tsum)
+
+if __name__ == '__main__':
+    sol = Solution()
+
+    nums = [4, 3, 2, 3, 5, 2, 1]
+    k = 4
+    r = sol.canPartitionKSubsets(nums, k)
+    print(r)
